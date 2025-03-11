fc1e964ab299453643ca72c220bbc1085bbc2750
KfH <85626296+kfh83@users.noreply.github.com>
2025-03-11 19:04:20 -0300

    Work CStartButton CPP, fix notations/prefixes on fields

--- src/StartButton.cpp
+++ src/StartButton.cpp
@@ -1,1 +1,45 @@
 #include "StartButton.h"
+
+#include <shlwapi.h>
+
+HRESULT CStartButton::QueryInterface(REFIID riid, void** ppvObject)
+{
+    static const QITAB qit[] =
+    {
+        QITABENT(CStartButton, IStartButton),
+        QITABENT(CStartButton, IServiceProvider),
+        { 0 },
+    };
+
+    return QISearch(this, qit, riid, ppvObject);
+}
+
+ULONG CStartButton::AddRef()
+{
+    return 1; // Copilot generated body
+}
+
+ULONG CStartButton::Release()
+{
+    return 1; // Copilot generated body
+}
+
+HRESULT CStartButton::SetFocusToStartButton()
+{
+    SetFocus(_hwndStartBtn);
+    return S_OK;
+}
+
+HRESULT CStartButton::SetStartPaneActive(BOOL bActive)
+{
+    if (bActive)
+    {
+        _nStartPaneActiveState = 1;
+    }
+    else if (_nStartPaneActiveState != 2)
+    {
+        _nStartPaneActiveState = 1;
+        UnlockStartPane();
+    }
+    return S_OK;
+}
